05af425978309e6bd043afacdfef84754fe9df80
leoarthurwall <leowall93@gmail.com>
2022-10-26 12:03:51 +0100

    created identifiable struct for the tasks

--- ToDoListSwift/File.swift
+++ ToDoListSwift/File.swift
@@ -9,3 +9,8 @@ import Foundation
 import SwiftUI
 import Combine
 
+// Identifiable - A class of types whose instances hold the value of an entity with stable identity.
+struct Task : Identifiable {
+    var id = String()
+    var todoItem = String()
+}
