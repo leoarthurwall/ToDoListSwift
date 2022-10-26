8620df2d2862782c2a5f65e8152d2bbb9ba43dc2
leoarthurwall <leowall93@gmail.com>
2022-10-26 11:35:00 +0100

    Initial Commit

---
+++ ToDoListSwift/ToDoListSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListSwiftApp.swift
+//  ToDoListSwift
+//
+//  Created by Leo Wall on 26/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
