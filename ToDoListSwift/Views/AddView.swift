1338f807f85b90abc7ee144cdcb22b80bc099211
leoarthurwall <leowall93@gmail.com>
2022-10-28 21:29:17 +0200

    created a new AddView view file and wrapped the preview in a navigationLink

---
+++ ToDoListSwift/Views/AddView.swift
@@ -0,0 +1,22 @@
+//
+//  AddView.swift
+//  ToDoListSwift
+//
+//  Created by Leo Wall on 28/10/22.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            AddView()
+        }
+    }
+}
