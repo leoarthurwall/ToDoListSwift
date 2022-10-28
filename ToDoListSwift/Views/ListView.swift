7f94a4c2dc4c186d9ca2317eb955c36e0ce4bf70
leoarthurwall <leowall93@gmail.com>
2022-10-28 20:26:22 +0200

    created a list, give it a title and some dummy text

--- ToDoListSwift/Views/ListView.swift
+++ ToDoListSwift/Views/ListView.swift
@@ -9,8 +9,10 @@ import SwiftUI
 
 struct ListView: View {
     var body: some View {
-        
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            Text("Hello, World!")
+        }
+        .navigationTitle("Todo List")
     }
 }
 
