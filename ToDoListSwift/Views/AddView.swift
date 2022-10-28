579620cc5b770da25ba637819f5992a169a3f2b0
leoarthurwall <leowall93@gmail.com>
2022-10-28 21:34:44 +0200

    created a textfield with text. created textFieldText state variable and set it to an empty string. added this state to the text section in the text field. so when something is written it can be added to this state variable

--- ToDoListSwift/Views/AddView.swift
+++ ToDoListSwift/Views/AddView.swift
@@ -8,8 +8,14 @@
 import SwiftUI
 
 struct AddView: View {
+    
+    @State var textFieldText: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            TextField("Type something here...", text: $textFieldText )
+        }
+        .navigationTitle("✚ Add an Item")
     }
 }
 
