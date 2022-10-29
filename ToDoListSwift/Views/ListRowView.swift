64674be16af1fe2198402095d4a3e970e7afc50b
leoarthurwall <leowall93@gmail.com>
2022-10-29 10:57:12 +0200

    set up a ternary operator for the ListRowView image icon. if item.iscompleted, display checked circle else display empty circle. Also, if item is completed make circle green else red.
    Also, gave padding to title and image and increased font size

--- ToDoListSwift/Views/ListRowView.swift
+++ ToDoListSwift/Views/ListRowView.swift
@@ -13,10 +13,13 @@ struct ListRowView: View {
     
     var body: some View {
         HStack{
-            Image(systemName: "checkmark.circle")
+            Image(systemName: item.isCompleted ? "checkmark.circle" : "circle")
+                .foregroundColor(item.isCompleted ? .green : .red)
             Text(item.title)
             Spacer()
         }
+        .font(.title2)
+        .padding(.vertical, 8)
     }
 }
 
