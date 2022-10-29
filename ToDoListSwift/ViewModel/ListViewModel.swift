63bc44788a9f857bcfd02319ee1b1595e37a6ed7
leoarthurwall <leowall93@gmail.com>
2022-10-29 12:13:26 +0200

    added the deleteItem and moveItem functions from list view to the view model file

--- ToDoListSwift/ViewModel/ListViewModel.swift
+++ ToDoListSwift/ViewModel/ListViewModel.swift
@@ -26,5 +26,11 @@ class ListViewModel {
         ]
         items.append(contentsOf: newItems)
     }
+    func deleteItem(indexSet: IndexSet) {
+        items.remove(atOffsets: indexSet)
+    }
+    func moveItem(from: IndexSet, to:Int) {
+        items.move(fromOffsets: from, toOffset: to)
+    }
     
 }
