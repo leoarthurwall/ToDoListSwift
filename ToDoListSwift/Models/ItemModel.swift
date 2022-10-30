51c117725b0b7a76bbc5f844f20dbd21277ecc68
leoarthurwall <leowall93@gmail.com>
2022-10-30 08:58:19 +0100

    customised item model so it doesn't create a new UUID when it's clicked (to change completion status). This was done by setting up an initialiser for id, title and isCompleted.  Also, with 'id: String = UUID().uuidString' as a parameter, we can we can initialise an ItemModel with an id if we give it a string, or without an id, and it will be automativally given one

--- ToDoListSwift/Models/ItemModel.swift
+++ ToDoListSwift/Models/ItemModel.swift
@@ -8,7 +8,15 @@
 import Foundation
 
 struct ItemModel: Identifiable {
-    let id: String = UUID().uuidString
+    let id: String
     let title: String
     let isCompleted : Bool
+    
+    // customise item model so it doesn't create a new UUID when it's clicked (to change completion status)
+    //with 'id: String = UUID().uuidString' as a parameter, we can initialise an ItemModel with an id if we give it a string, or without an id, and it will be automativally given one
+    init(id: String = UUID().uuidString, title:String, isCompleted: Bool) {
+        self.id = id
+        self.title = title
+        self.isCompleted = isCompleted
+    }
 }
