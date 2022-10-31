e91f10a1dca5550537242ac4aa7729b63d270660
leoarthurwall <leowall93@gmail.com>
2022-10-31 12:35:19 +0100

    created a NoItemsView file where the UI for the No Items View will be set

---
+++ ToDoListSwift/Views/NoItemsView.swift
@@ -0,0 +1,20 @@
+//
+//  NoItemsView.swift
+//  ToDoListSwift
+//
+//  Created by Leo Wall on 31/10/22.
+//
+
+import SwiftUI
+
+struct NoItemsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NoItemsView_Previews: PreviewProvider {
+    static var previews: some View {
+        NoItemsView()
+    }
+}
