6bd218ed89b0d2441c7956e0d2bfd02ca9121930
leoarthurwall <leowall93@gmail.com>
2022-10-31 14:45:32 +0100

    added the NoItemsView() to the list view. so now this view displays when no items are entered

--- ToDoListSwift/Views/ListView.swift
+++ ToDoListSwift/Views/ListView.swift
@@ -14,7 +14,7 @@ struct ListView: View {
     var body: some View {
         ZStack {
             if listViewModel.items.isEmpty {
-                Text("No Items")
+                NoItemsView()
             } else {
                 List {
                     ForEach( listViewModel.items) { item in
